3f1bf5fe3e6c220ecc3482ef54674a6500f86828
UrbSen <u.senica@gmail.com>
2016-07-12 15:15:10 +0200

    Print another line

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main()
 {
 	char name[20];
-  cout<<"Name, please?";
+  	cout<<"Name, please?";
 	cin>>name;
 	cin.ignore();
-  cout<<"Greetings "<<name<<", welcome to Urban's first C++ program!\n"<< "Hello World!";
+  	cout<<"Greetings "<<name<<", welcome to Urban's first C++ program!\n"<< "Hello World!";
+    cout<<"And this is another wonderful addition!\n";
   cin.get();
 }
